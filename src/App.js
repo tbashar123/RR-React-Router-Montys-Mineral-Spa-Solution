8bd0551ab5b3d2f0a4d482317da29430f4becddf
Tanvir Bashar <tbashar09@gmail.com>
2022-08-29 13:30:30 -0400

    updated project

--- src/App.js
+++ src/App.js
@@ -5,46 +5,22 @@ import './App.css';
 import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
+
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 
-
-
-
 function App() {
-  
+
   const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
 
-   
-  return (
-    
-    <div className="App">
-      
 
+  return (
+    <div className="App">
       <Router>
-      <Container>
-    <Nav defaultActiveKey="/" variant="tabs" fill>
-        <Nav.Item>
-            <Nav.Link href="/" eventKey={"homePage"}> 
-                <Link to="/">Home</Link>
-            </Nav.Link>
-        </Nav.Item>
-        <Nav.Item >
-            <Nav.Link eventKey={"aboutPage"}> 
-                <Link to="/about">About Us</Link>
-            </Nav.Link>
-        </Nav.Item>
-        <Nav.Item >
-            <Nav.Link eventKey={"packagesPage"}> 
-                <Link to="/packages">Our Packages</Link> 
-            </Nav.Link>
-         </Nav.Item>
-    </Nav>
-</Container>
         <header>
           <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
-          <div className="navBar">
+          {/* <div className="navBar">
             <ul>
               <li>
                 <Link to="/">Home</Link>
@@ -56,13 +32,26 @@ function App() {
                 <Link to="/packages">Our Packages</Link>
               </li>
             </ul>
-          </div>
+          </div> */}
+          <Container>
+            <Nav defaultActiveKey="/" variant="tabs" fill>
+              <Nav.Item>
+                <Nav.Link href="/"> <Link to="/">Home</Link> </Nav.Link>
+              </Nav.Item>
+              <Nav.Item >
+                <Nav.Link eventKey={"aboutPage"}> <Link to="/about">About Us</Link> </Nav.Link>
+              </Nav.Item>
+              <Nav.Item >
+                <Nav.Link eventKey={"packagesPage"}> <Link to="/packages">Our Packages</Link> </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Container>
         </header>
 
         <div className="display">
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
-          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
+          <Route path="/packages" render={() => <Packages packages={packages} />} />
         </div>
 
       </Router>
@@ -72,6 +61,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
